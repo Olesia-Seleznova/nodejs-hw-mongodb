64622d5eb994995069d847ec441efba06b950fe1
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-06-14 19:00:32 +0300

    add contacts.js, modified server.js

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,29 +1,33 @@
-import { model, Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  email: {
-    type: String,
-    required: false,
-  },
-  isFavourite: {
-    type: boolean,
-    required: false,
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    default: 'personal',
-  },
-  timestamps: true,
-  versionKey: false,
-});
+);
 
-export const ContactsCollection = model('contacts', contactsSchema);
+export const ContactsCollection = model('contacts', contactSchema);
