35fefe092495c27d5d3e254573054392b26b4314
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-07-07 18:03:45 +0300

    add signoin, signup

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import createHttpError from 'http-errors';
 import { signup, findUser } from '../services/auth.js';
+import { compareHash } from '../utils/hash.js';
 
 export const signupController = async (req, res) => {
   const { email } = req.body;
@@ -11,14 +12,36 @@ export const signupController = async (req, res) => {
 
   const newUser = await signup(req.body);
 
-  data = {
+  const data = {
     name: newUser.name,
     email: newUser.email,
   };
 
   res.status(201).json({
     status: 201,
+    data,
     message: 'Successfully registered a user!',
-    data: newUser,
+  });
+};
+
+export const signinController = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findUser({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Email invalid');
+  }
+
+  const passwordCompare = await compareHash(password, user.password);
+  if (!passwordCompare) {
+    throw createHttpError(401, 'Password invalid');
+  }
+
+  const accsessToken = '122.4q52.44444';
+  const refreshToken = '2132.4151.33';
+
+  res.json({
+    accsessToken,
+    refreshToken,
   });
 };
